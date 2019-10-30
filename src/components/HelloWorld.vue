b108b1e8fee4f033d3c152687a551cabdc0cc4b3
ppn <ponponner@gmail.com>
2019-10-30 23:19:15 +0900

    [etc] fontawesome導入

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,9 @@
 
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">
+          <v-icon>fa-grin-stars</v-icon>
           Welcome to Vuetify
+          <v-icon>fa-grin-stars</v-icon>
         </h1>
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
